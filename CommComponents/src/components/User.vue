dda0a325b4abdb656522170b2798aa5a6b25b6ee
Eric Siu <eric.ay.siu@gmail.com>
2018-10-06 23:38:37 +0800

    finished comm components section

--- CommComponents/src/components/User.vue
+++ CommComponents/src/components/User.vue
@@ -2,23 +2,49 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
+        
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail 
+                    :userName="name" 
+                    :userAge="age"
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName">
+                </app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEdited="age = $event"></app-user-edit>
             </div>
         </div>
     </div>
 </template>
-
+ 
 <script>
+    
     import UserDetail from './UserDetail.vue';
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function() {
+            return {
+                name: 'Eric',
+                age: 43
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Anna';
+            },
+            resetName() {
+                this.name = 'Eric';
+            },
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- CommComponents/src/components/UserDetail.vue
+++ CommComponents/src/components/UserDetail.vue
@@ -2,10 +2,53 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }} </p>
+        <p>User Age: {{ userAge }} </p>
+        <button @click="resetName">Reset name</button>
+        <button @click="resetFn()"> Reset name (parent)</button>
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../main';
+    export default{
+        props: {
+            userName: {
+                type: String
+            },
+            resetFn: Function,
+            userAge: Number,
+            sampleStringProp: {
+                type: String,
+                default: 'This is a default string value'
+                
+            },
+            sampleStringProp2: {
+                type: Object,
+                default: function() {
+                    return {
+                        name: 'Eric',
+                        age: 43
+                    }
+                }
+            }
+        },
+        methods: {
+            switchName() {
+                return this.userName.split("").reverse().join("");
+            },
+            resetName() {
+                this.userName = 'Eric';
+                this.$emit('nameWasReset', this.myName);
+            }
+        },
+        created() {
+            eventBus.$on('ageWasEdited', (age) => {
+                this.userAge = age;
+            });
+
+        }
+    }
 </script>
 
 <style scoped>
