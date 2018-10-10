fdf42b6ade7877347dcfd27c300620c08e28d41f
Eric Siu <eric.ay.siu@gmail.com>
2018-10-10 23:55:09 +0800

    complete AdvancedComponents before starting WonderfulQuotes

---
+++ AdvancedComponents/src/components/New.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{ counter }}</p>
+    </div>    
+</template>
+
+<script>
+    export default { 
+        data: function() {
+            return {
+                counter: 0
+            };
+        },
+        destroyed() {
+            console.log('appNew - destroyed!!');
+        },
+        deactivated() {
+            console.log('appNew - deactivated!!');
+        },
+        activated() {
+            console.log('appNew - activated!!');
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ AdvancedComponents/src/components/Quote.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <!-- <p>{{ quote }}</p>     -->
+        <div class="title">
+            <slot name='title'></slot>
+            <span style="color: #ccc"><slot name='subtitle'>The Subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+            <slot></slot>
+        </div>
+        
+    </div>    
+</template>
+
+<script>
+export default {
+    // props: ['quote']
+
+
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 5px gray;
+        padding: 20px;
+        margin: 50px auto;
+        text-align: center;
+    }
+
+    h2 {
+        color: red;
+    }
+
+    .title {
+        font-style: italic;
+    }
+</style>
