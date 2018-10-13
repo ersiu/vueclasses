554cfd19fe472daa21d54d5ce023be7865ac62ea
Eric Siu <eric.ay.siu@gmail.com>
2018-10-13 15:22:27 +0800

    finished custom directives

---
+++ UnderstandDirectives/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.background = 'green';
+    // el.style.backgroundColor = binding.value;
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+    
+  }
+}); 
+
+// 5 Hooks
+// bind hook
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
