18916bbbf50508e09608f1421a5102ccb04089ff
Eric Siu <eric.ay.siu@gmail.com>
2018-10-14 22:44:40 +0800

    finished Filters and Mixins

---
+++ FilterMixin/src/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin } from './fruitMixin'; 
+    export default {
+        mixins: [fruitMixin],
+        created() {
+            console.log('Created: Inside List');
+        }
+    }
+</script>
+
+<style>
+
+</style>
